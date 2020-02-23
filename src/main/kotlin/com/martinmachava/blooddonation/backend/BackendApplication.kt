2ac8a3f86fb2d98ce8b07cdeafcfc0f72b5fcdbf
Martin Machava <Vergil333@gmail.com>
2020-02-23 16:39:35 +0100

    import spring boot project

---
+++ src/main/kotlin/com/martinmachava/blooddonation/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package com.martinmachava.blooddonation.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+}
