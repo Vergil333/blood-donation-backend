64944d9823e1ec772d7b5776b10b2969a1365f9f
Martin Machava <Vergil333@gmail.com>
2020-03-16 14:33:55 +0700

    access rest api to everyone

--- src/main/kotlin/com/martinmachava/blooddonation/backend/config/SpringSecurityConfig.kt
+++ src/main/kotlin/com/martinmachava/blooddonation/backend/config/SpringSecurityConfig.kt
@@ -13,8 +13,6 @@ open class SpringSecurityConfig: WebSecurityConfigurerAdapter() {
 
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.inMemoryAuthentication()
-                .withUser("demoUSER").password("{noop}demoPASSWORD").roles("USER")
-                .and()
                 .withUser("adminUSER").password("{noop}adminPASSWORD").roles("ADMIN")
     }
 
@@ -22,8 +20,6 @@ open class SpringSecurityConfig: WebSecurityConfigurerAdapter() {
         http.httpBasic()
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/donor").authenticated()
-                .antMatchers(HttpMethod.POST, "/donor").authenticated()
                 .antMatchers(HttpMethod.DELETE, "/donor").hasRole("ADMIN")
                 .and().csrf().disable()
     }
